32f58dbac0fbf7d40b3e02811fc629b52ed83cbe
Yeshwant Thota <74702686+yeshwantthota@users.noreply.github.com>
2025-01-31 08:04:44 +0530

    Update Easy.js

--- Javascript/Core-JS/Easy.js
+++ Javascript/Core-JS/Easy.js
@@ -70,4 +70,19 @@
   console.log(d);
 }
 
+/* what is the output?
+myfun();
+
+var myfun = function (){
+    console.log("first")
+}
+
+myfun();
+
+function myfun(){
+    console.log("second")
+}
+myfun()
+**/
+
 //end-2
